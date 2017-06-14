da82f5c8ec084ae52b3d7d487fcea76d87dd03af
Jesse Brent Coars <jbcoats@users.noreply.github.com>
2017-06-14 05:40:16 -0700

    Add files via upload

---
+++ Controllers/MaintenancesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MetroAltWebApplication.Models;
+
+namespace MetroAltWebApplication.Controllers
+{
+    public class MaintenancesController : Controller
+    {
+        private MetroAltEntities db = new MetroAltEntities();
+
+        // GET: Maintenances
+        public ActionResult Index()
+        {
+            return View(db.Maintenances.ToList());
+        }
+
+        // GET: Maintenances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maintenance maintenance = db.Maintenances.Find(id);
+            if (maintenance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maintenance);
+        }
+
+        // GET: Maintenances/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Maintenances/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaintenanceKey,MaintenanceDate,BusKey")] Maintenance maintenance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Maintenances.Add(maintenance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(maintenance);
+        }
+
+        // GET: Maintenances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maintenance maintenance = db.Maintenances.Find(id);
+            if (maintenance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maintenance);
+        }
+
+        // POST: Maintenances/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaintenanceKey,MaintenanceDate,BusKey")] Maintenance maintenance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(maintenance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(maintenance);
+        }
+
+        // GET: Maintenances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Maintenance maintenance = db.Maintenances.Find(id);
+            if (maintenance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(maintenance);
+        }
+
+        // POST: Maintenances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Maintenance maintenance = db.Maintenances.Find(id);
+            db.Maintenances.Remove(maintenance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
